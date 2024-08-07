83f68b1052e70adca7968f2794560b3076ee7da0
SveTi4 <studymail2.7.53@gmail.com>
2024-08-07 09:32:33 +0700

    Add webpack html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.js'),
@@ -7,5 +9,8 @@ module.exports = {
         path: path.resolve(__dirname, 'build'),
         filename: '[name].[contenthash].js',
         clean: true
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+    ]
 }
