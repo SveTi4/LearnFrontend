22253e813b888bcef1581301c648b1f906c4d1ab
SveTi4 <studymail2.7.53@gmail.com>
2024-08-07 13:47:37 +0700

    Edit webpack config for ts

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
 export function someFn(string: number): string {
     console.log('hello world');
 
-    return `Your number is ${string}`
+    return "1"
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,32 +1,33 @@
-const path = require('path');
+import path from 'path';
+import webpack from 'webpack';
+import HTMLWebpackPlugin from "html-webpack-plugin";
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
-
-module.exports = {
+const config: webpack.Configuration = {
     mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.ts'),
     output: {
+        filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, 'build'),
-        filename: '[name].[contenthash].js',
         clean: true
     },
     plugins: [
-        new HtmlWebpackPlugin({
+        new HTMLWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
         }),
-        new webpack.ProgressPlugin()
+        new webpack.ProgressPlugin(),
     ],
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
-                exclude: /node_modules/
+                exclude: /node_modules/,
             }
-        ]
+        ],
     },
     resolve: {
-        extensions: ['tsx', '.ts', '.js']
-    }
+        extensions: ['.tsx', '.ts', '.js'],
+    },
 }
+
+export default config;
