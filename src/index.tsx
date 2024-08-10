8deee20cd3ae7b4732a714b8b9922e5b829a28b0
SveTi4 <studymail2.7.53@gmail.com>
2024-08-11 01:29:56 +0700

    Add react and react-dom

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import { render } from 'react-dom';
+import React from 'react'
+
+render(
+    <div>
+        <h1>Hello, world!</h1>
+    </div>,
+    document.getElementById('root')
+)
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-export function someFn(string: number): string {
-    console.log('hello world');
-
-    return "1"
-}
