3200f9b76179d24ba1519ba828927a8a48953d0e
SveTi4 <studymail2.7.53@gmail.com>
2024-08-11 00:42:57 +0700

    Add env and edit scripts for start/build

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -1,9 +1,15 @@
 export type BuildMode = 'production' | 'development';
-export type BuildPaths = {
+export interface BuildPaths {
     entry: string;
     build: string;
     html: string;
 }
+
+export interface BuildEnv {
+    mode: BuildMode;
+    port: number;
+}
+
 export interface BuildOptions {
     mode: BuildMode;
     paths: BuildPaths;
