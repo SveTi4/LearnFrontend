65b761b5cf9b4d480242e1110460373934161466
SveTi4 <studymail2.7.53@gmail.com>
2024-08-15 16:13:59 +0700

    Add mini-css-extract-plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,13 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
+import { BuildOptions } from './types/config';
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          "style-loader",
+          options.isDev ? "style-loader" : MiniCssExtractPlugin.loader,
           // Translates CSS into CommonJS
           "css-loader",
           // Compiles Sass to CSS
