776f9b76c657827c1ce037f07633fb54ec607953
SveTi4 <studymail2.7.53@gmail.com>
2024-08-16 16:21:37 +0700

    Add async imports and suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-import AboutPage from "./components/pages/AboutPage/AboutPage";
-import MainPage from "./components/pages/MainPage/MainPage";
 import { Link } from "react-router-dom";
+import AboutPageAsync from "./components/pages/AboutPage/AboutPage.async";
+import MainPageAsync from "./components/pages/MainPage/MainPage.async";
+import { Suspense } from "react";
 
 const App = () => {
     return (
         <div className="app">
             <Link to={'/about'}>About</Link>
             <Link to={'/'}>Main</Link>
-            <Routes>
-                <Route path={'/about'} element={<AboutPage />}/>
-                <Route path={'/'} element={<MainPage />}/>
-            </Routes>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />}/>
+                    <Route path={'/'} element={<MainPageAsync />}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
